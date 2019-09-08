a0e21bb09902316a8263f6d93b8d5234ce22133b
Iryna Dobushovska <i.dobushovska@gmail.com>
2019-09-08 11:38:07 -0700

    Added login/logout decorators for base functions

--- src/base.py
+++ src/base.py
@@ -3,20 +3,58 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
+def login_decorator(func):
+    def login_to_website(*args, **kwargs):
+        driver = args[0].driver
+        current_url = driver.current_url
+        login_buttons = driver.find_elements_by_class_name('login')
+        if len(login_buttons) > 0:
+            login_buttons[0].click()
+            inp_email = driver.find_element_by_id('email')
+            inp_pass = driver.find_element_by_id('passwd')
+            btn_login = driver.find_element_by_id('SubmitLogin')
+            inp_email.send_keys("test21@email.com")
+            inp_pass.send_keys("test1234")
+            btn_login.click()
+            driver.get(current_url)
+        func(*args, **kwargs)
+    return login_to_website
+
+
+def anonymous_decorator(func):
+    def logout_from_website(*args, **kwargs):
+        driver = args[0].driver
+        current_url = driver.current_url
+        logout_buttons = driver.find_elements_by_class_name('logout')
+        if len(logout_buttons) > 0:
+            logout_buttons[0].click()
+            driver.get(current_url)
+        func(*args, **kwargs)
+    return logout_from_website
+
+
 class HomePageTest(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome(executable_path='../../bin/chromedriver')
+        self.driver.get("http://automationpractice.com/index.php")
 
-    def test_open_home_page(self):
+    def search_from_home_page(self):
         driver = self.driver
-        driver.get("http://automationpractice.com/index.php")
         self.assertIn("My Store", driver.title)
         elem = driver.find_element_by_id("search_query_top")
         elem.send_keys("CHIFFON")
         elem.send_keys(Keys.RETURN)
         products = driver.find_elements_by_class_name("product-container")
         assert len(products) == 2
+
+    @login_decorator
+    def test_search_from_home_page_as_logged_user(self):
+        self.search_from_home_page()
+
+    @anonymous_decorator
+    def test_search_from_home_page_as_guest_user(self):
+        self.search_from_home_page()
 
     def tearDown(self):
         self.driver.close()
