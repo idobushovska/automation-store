97e53a728f644f2987cba2f21159a5dcad2f7fb0
Iryna Dobushovska <i.dobushovska@gmail.com>
2019-09-12 08:14:26 -0700

    Added test for checkout

--- test_case_critical_path.py
+++ test_case_critical_path.py
@@ -3,12 +3,13 @@
 import unittest
 import time
 from selenium import webdriver
+import re
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 import settings
-from helpers import auth
-from helpers import actions
+from helpers import auth, actions, selectors
 
 
 class CriticalPathTest(unittest.TestCase):
@@ -19,7 +20,7 @@ class CriticalPathTest(unittest.TestCase):
     4. Shopping Cart.
     5. Checkout.
     6. View order status."""
-    @unittest.skip('skip')
+    unittest.skip('skip')
     @auth.anonymous
     def test_register_user(self):
         driver = self.driver
@@ -85,14 +86,62 @@ class CriticalPathTest(unittest.TestCase):
         )
         assert quantity_items_in_cart_span.text == '2'
 
-    @unittest.skip('skip')
     @auth.logged_customer
     def test_checkout(self):
-        pass
+        driver = self.driver
+        wait = self.wait
+        products = selectors.get_all_products_on_page(driver)
+        actions.add_product_to_cart(driver, wait, products[0])
+        checkout_button = self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="header"]/div[3]/div/div/div[3]/div/a'))
+        )
+        checkout_button.click()
+
+        proceed_to_address_button = self.wait.until(
+            EC.element_to_be_clickable((By.CLASS_NAME, 'standard-checkout'))
+        )
+        proceed_to_address_button.click()
+
+        proceed_to_shipping_button = self.wait.until(
+            EC.element_to_be_clickable((By.NAME, 'processAddress'))
+        )
+        proceed_to_shipping_button.click()
+
+        agree_to_shipping_checkbox = self.wait.until(
+            EC.visibility_of_element_located((By.ID, 'uniform-cgv'))
+        )
+        agree_to_shipping_checkbox.click()
+
+        proceed_to_payment_button = self.wait.until(
+            EC.element_to_be_clickable((By.NAME, 'processCarrier'))
+        )
+        proceed_to_payment_button.click()
+
+        pay_by_bankwire_button = self.wait.until(
+            EC.element_to_be_clickable((By.CLASS_NAME, 'bankwire'))
+        )
+        pay_by_bankwire_button.click()
+
+        complete_order_button = self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="cart_navigation"]/button'))
+        )
+        complete_order_button.click()
+
+        confirmation_title = self.wait.until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="center_column"]/div/p/strong'))
+        )
+
+        assert confirmation_title.text == 'Your order on My Store is complete.'
+
+        confirmation_message = self.driver.find_element_by_xpath('//*[@id="center_column"]/div')
+        order_number = re.findall("[0-9A-Z]{9}", confirmation_message.text)[0]
+
+        assert len(order_number) == 9
+        self.order_number = order_number
 
     @unittest.skip('skip')
     @auth.logged_customer
-    def verify_order(self):
+    def test_verify_order(self):
         pass
 
     def setUp(self):
