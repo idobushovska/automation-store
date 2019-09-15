7b158bb4bb0134730767ed38235c8264afeb62cf
Iryna Dobushovska <i.dobushovska@gmail.com>
2019-09-14 23:34:12 -0700

    Updated README and settings files

--- settings.py
+++ settings.py
@@ -3,20 +3,20 @@
 config = {
     "website": {
         "homepage": "http://automationpractice.com/index.php",
-        "email": "test21@email.com",
+        "email": "test23@email.com",
         "passwd": "test1234",
         "new_customer": {
-            "firstname": "Greg",
+            "firstname": "Gregory",
             "lastname": "House",
             "company": "Ira's networks",
             "address": "5234 Santana Row",
             "city": "Mountain View",
             "state": "California",
-            "zip": "90344",
-            "mobile": "209000001",
+            "zip": "94043",
+            "mobile": "4083843847",
             "email_pattern": "as_{}@autostore.com",
             "passwd": "test1234$",
-            "dob": "02/24/1987"
+            "dob": "03/13/1987"
         }
     },
     "selenium": {
